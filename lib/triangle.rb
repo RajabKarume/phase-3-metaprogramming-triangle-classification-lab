f37eae2a7dfb700f2f3e86341a153ba1019d6e1b
Rajab <rkarume@gmail.com>
2022-10-28 00:56:39 +0300

    commit tree 0f41ca522da0658ed1385978e51874c8cd8d8e5b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :length, :width, :height
+  def initialize(length, width, height)
+    @length = length
+    @width = width
+    @height = height
+  end
+  def kind
+    if is_a_triangle?
+      if @length == @width && @height == @length
+        :equilateral
+      elsif @length == @width || @width == @height || @length== @height
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+  def is_a_triangle?
+    length > 0 && width > 0 && height > 0
+    length < height + width && height < length + width && width < length + height
+  end
+  class TriangleError < StandardError
+    
+  end
 end
